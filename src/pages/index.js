bfbc9827039e93ed819b0702c9d9d9c04cbf5ebf
Robbie McKinstry <robbie@hashicorp.com>
2017-09-18 10:45:07 -0400

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Homework</h1>
+    <ul>
+        <li>
+            <p>Lab 6: Using Java Arrays</p>
+            <p>Due Friday, 9/22 </p>
+        </li>
+        <li>
+            <p>Introduction to Java, <i>Gatis</i>, Chapter 7 #17</p>
+            <p>Due Tuesday, 9/26 </p>
+        </li>
+    </ul>
+  </div>
+)
+
+export default IndexPage
